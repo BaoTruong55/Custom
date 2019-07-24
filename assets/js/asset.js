87d9c1c507ddd54ae94216909a69a75fd5018e13
Bao intern ICT DEV <intern.ict003@itl.com>
2019-07-24 17:46:50 +0700

    day2

--- assets/js/asset.js
+++ assets/js/asset.js
@@ -1,24 +1,36 @@
+//!Chart
 var ctx = document.getElementById("myChart").getContext("2d");
+var gradientStroke = ctx.createLinearGradient(500, 0, 100, 0);
+gradientStroke.addColorStop(0, "#02afd1");
+
+gradientStroke.addColorStop(1, "#6ae6a7");
 var myChart = new Chart(ctx, {
   type: "line",
   data: {
     labels: ["1 Dec", "8 Dec", "16 Dec", "31 Dec"],
     datasets: [
       {
-        label: "Earnings",
+        label: "Monthly",
         lineTension: 0.3,
         backgroundColor: "rgba(78, 115, 223, 0.05)",
-        borderColor: "rgba(78, 115, 223, 1)",
-        pointRadius: 3,
-        pointBackgroundColor: "rgba(78, 115, 223, 1)",
-        pointBorderColor: "rgba(78, 115, 223, 1)",
+        borderColor: gradientStroke,
+        pointBorderColor: gradientStroke,
+        pointBackgroundColor: gradientStroke,
+        pointHoverBackgroundColor: gradientStroke,
+        pointHoverBorderColor: gradientStroke,
         pointHoverRadius: 3,
         pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
-        pointHoverBorderColor: "rgba(78, 115, 223, 1)",
         pointHitRadius: 10,
         pointBorderWidth: 2,
         data: [12, 19, 3, 17, 6, 3, 7]
       }
     ]
   }
+});
+
+//!Select
+$("#tableMenu a").click(function(e) {
+  e.preventDefault();
+  var selText = $(this).text();
+  $("#tableButton").text(selText);
 });
